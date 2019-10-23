4db6b5bb9effd18dd1dad9bcda5eccec8c67f895
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-23 13:24:36 -0500

    finished

--- client/src/components/fileUpload.js
+++ client/src/components/fileUpload.js
@@ -1,10 +1,15 @@
 import React, {Fragment, useState} from 'react';
+import Message from './Message';
+import Progress from './Progress';
 import axios from 'axios';
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose File');
     const [uploadedFile, setUploadedFile] = useState({});
+    const [message, setMessage] = useState('');
+    const [uploadPercentage, setUploadPercentage] = useState(0);
+
 
     const onChange = e => {
         setFile(e.target.files[0]);
@@ -21,17 +26,27 @@ const FileUpload = () => {
             const res = await axios.post('/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: progressEvent => {
+                    setUploadPercentage(parseInt(Math.round(progressEvent.loaded * 100)/ progressEvent.total))
+
+                                    //Clear Percentage
+                setTimeout(()=> setUploadPercentage(0), 10000);
                 }
+
+
             });
 
             const {fileName, filePath} = res.data;
 
             setUploadedFile({fileName, filePath});
+
+            setMessage('File Uploaded');
         }catch(err) {
             if(err.response.status === 500) {
-                console.log('There was a problem with the server.')
+                setMessage('There was a problem with the server.')
             }else {
-                console.log(err.response.data.msg);
+                setMessage(err.response.data.msg);
             }
         }
     }
@@ -39,6 +54,7 @@ const FileUpload = () => {
 
     return (
         <Fragment>
+            { message ? <Message msg={message}/> : null }
             <form onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
@@ -47,8 +63,21 @@ const FileUpload = () => {
                     </label>
                 </div>
 
-                <input type='submit' value='upload' className='btn btn-primary btn-block mt-4'/>
+                <Progress percentage={uploadPercentage} />
+
+                <input 
+                    type='submit' 
+                    value='upload' 
+                    className='btn btn-primary btn-block mt-4'
+                />
+                { uploadedFile ? <div className='row mt-5'>
+                    <div className='col-md-6 m-auto'>
+                        <h3 className='text-center'>{ uploadedFile.fileName }</h3>
+                        <img style={{ width: '100%' }}src={uploadedFile.filePath} alt=''/>
+                    </div>
+                </div> : null }
             </form>
+
         </Fragment>
     )
 }
